3f028bbd54dfee58629337905aeace189069b076
Michiel Borkent <michielborkent@gmail.com>
2020-04-22 15:31:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Generate podcast
+
+A [babashka](https://github.com/borkdude/babashka/) script to transform a
+directory of mp3 files into a XML file that can read by iTunes to subscribe as
+podcast.
+
+This is a great way to enjoy conference talks on your iPod or mobile phone.
+
+## Usage
+
+- Place the script `generate_podcast.clj` somewhere on your path.
+
+- Create a directory with `.mp3` files. E.g. download [Clojure Conj
+2019](https://www.youtube.com/watch?v=MnvtPzEH-d8&list=PLZdCLR02grLqSy15ALLAZDU6LGpAJDrAQ)
+as mp3 files with [youtube-dl](https://github.com/ytdl-org/youtube-dl).
+
+  ``` shell
+  $ mkdir -p conj2019 && cd conj2019
+  $ youtube-dl -x --audio-format mp3 "https://www.youtube.com/watch?v=MnvtPzEH-d8&list=PLZdCLR02grLqSy15ALLAZDU6LGpAJDrAQ"
+  ```
+
+- Start a local webserver in the mp3-files directory
+
+   ``` shell
+   $ python -m SimpleHTTPServer
+   ```
+
+- In another session generate the podcast file from inside the mp3-files directory:
+
+   ``` shell
+   $ generate_podcast.clj -h
+   podcast.clj:
+
+   -f --file FILE Podcast file to generate :default podcast.xml
+   -t --title TITLE Title for the podcast
+   -b --base-url URL Base URL of server
+   -h --help
+   $ generate_podcast.clj -p conj2019.xml -b http://localhost:8000 -t "Clojure Conj 2019"
+   Written podcast for Clojure Conj 2019 at podcast.xml
+   ```
+
+- In iTunes, subscribe to the podcast: `File > Subscribe to podcast > http://localhost:8000/conj2019.xml`.
+
+Happy listening!
+
+<img src="nano.jpg"/>
+
+## License
+
+Copyright © 2020 Michiel Borkent
+
+Distributed under the EPL License. See LICENSE.
---
+++ generate_podcast.clj
@@ -0,0 +1,62 @@
+#!/usr/bin/env bb
+
+(require '[clojure.java.io :as io]
+         '[clojure.data.xml :as xml]
+         '[clojure.string :as str]
+         '[clojure.tools.cli :as cli])
+(import '[java.nio.file Files LinkOption])
+
+(defn created [^java.io.File f]
+  (-> (.toPath f)
+      (Files/getAttribute  "creationTime" (into-array LinkOption []))
+      str))
+
+(defn item [^java.io.File f base-url]
+  (xml/element
+   :item
+   {}
+   (xml/element :title {} (.getName f))
+   (xml/element :pubDate {} (created f))
+   (xml/element :enclosure {:url (str base-url "/"
+                                      (-> (.getName f)
+                                          (java.net.URLEncoder/encode)
+                                          (str/replace "+" "%20")))
+                            :type "audio/mpeg"})))
+
+(defn rss [title base-url podcast-file]
+  (xml/element
+   :rss {}
+   (xml/element
+    :channel {:version "2.0"
+              :xmlns/itunes "http://www.itunes.com/dtds/podcast-1.0.dtd"}
+    (xml/element :title {} title)
+    (xml/element :link {} (str base-url "/" podcast-file))
+    (for [f (file-seq (io/file "."))]
+      (item f base-url)))))
+
+(def cli-options
+  [["-f" "--file FILE" "Podcast file to generate"
+    :default "podcast.xml"]
+   ["-t" "--title TITLE" "Title for the podcast"]
+   ["-b" "--base-url URL" "Base URL of server"]
+   ["-h" "--help"]])
+
+(defn print-help
+  []
+  (println "podcast.clj:")
+  (println)
+  (doseq [o cli-options]
+    (apply println o)))
+
+(let [args (cli/parse-opts *command-line-args* cli-options)
+      {:keys [:file :base-url :title :help]} (:options args)]
+  (if (or (not file)
+          (not base-url)
+          (not title)
+          help)
+    (print-help)
+    (let [base-url (str/replace base-url #"/*$" "")]
+      (spit file
+            (xml/emit-str
+             (rss title base-url file)))
+      (println "Written podcast for" title "at" file))))
